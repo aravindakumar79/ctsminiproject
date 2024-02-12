79964396d61cd4fb8e9de45e42d49674ffc014d5
Aravinda Kumar V <aravindakumar9@hotmail.com>
2024-02-12 13:00:49 +0530

    new file cts

--- cts.py
+++ cts.py
@@ -1,2 +1,1 @@
-print ("welcome to python cts mini project")
-after new update output"[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D"([C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C)[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dprint ("after new update in jenkins")
+print ("after removed cts file")
